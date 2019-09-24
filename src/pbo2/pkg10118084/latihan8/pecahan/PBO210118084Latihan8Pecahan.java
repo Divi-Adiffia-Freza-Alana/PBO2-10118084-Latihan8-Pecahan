0e0b91667e6b78931d26a61f0abd503c60751175
Divi-Adiffia-Freza-Alana <55723716+Divi-Adiffia-Freza-Alana@users.noreply.github.com>
2019-09-24 13:32:40 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118084/latihan8/pecahan/PBO210118084Latihan8Pecahan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118084.latihan8.pecahan;
+
+/**
+ *
+ * @author
+ *Nama  : Divi Adiffia Freza Alana
+ *Kelas : PBO2
+ *Nim   : 10118084
+ *Deskripsi Program : Program ini berisi program untuk menampilkan Pecahan
+
+ */
+public class PBO210118084Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float :"+f);
+        System.out.println("Double :" + d);
+        // TODO code application logic here
+    }
+    
+}
